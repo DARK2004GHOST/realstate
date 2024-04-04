0820ec657f0675b7844cb68c1b87c26072065051
DARK2004GHOST <127779876+DARK2004GHOST@users.noreply.github.com>
2024-04-04 17:47:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Incluir el archivo de conexión
-include '/crudinmuebles/conexion.php';
+include 'conexion.php';
 
 // Verificar si se ha enviado un formulario de inicio de sesión
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
