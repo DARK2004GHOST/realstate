555582b52d0d9ad8ce265af3dcebd048785b2a0e
DARK2004GHOST <127779876+DARK2004GHOST@users.noreply.github.com>
2024-04-04 17:04:20 -0500

    Update conexion.php

--- crudcitas/conexion.php
+++ crudcitas/conexion.php
@@ -1,16 +1,13 @@
 <?php
 // Definición de los datos de conexión
-$servername="localhost";  // Dirección del servidor de la base de datos
-$database="realstate";     // Nombre de la base de datos
-$username="root";          // Nombre de usuario de la base de datos
-$pasaword="";              // Contraseña de la base de datos (en este caso, vacía)
+$servername="realstate.database.windows.net";
+$database="realstate";
+$username="Aprendiz";
+$password="Andres2004";
 
-// Creación de la conexión utilizando los datos definidos anteriormente
-$con=  new mysqli("$servername", "$username", "$pasaword", "$database");
+    $con=  new mysqli("$servername", "$username", "$password", "$database");
 
-// Verificación de la conexión
-if($con -> connect_error ) {
-    // En caso de error, se muestra un mensaje indicando la falla y se detiene la ejecución
-    die("FALLA EN LA CONEXION " . $con -> connect_error) ; 
-}
+    if($con -> connect_error ) {
+        die("FALLA EN LA CONEXION " . $con -> connect_error) ; 
+    }
 ?>
