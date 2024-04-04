454228fe051ace2d4ab31b679db34cf25a217715
DARK2004GHOST <127779876+DARK2004GHOST@users.noreply.github.com>
2024-04-04 17:05:39 -0500

    Update conexion.php

--- crudusuarios/conexion.php
+++ crudusuarios/conexion.php
@@ -1,14 +1,13 @@
 <?php
-//   http://localhost/crudrealstate/conexion.php -->
-$servername="localhost";
+// Definición de los datos de conexión
+$servername="realstate.database.windows.net";
 $database="realstate";
-$username="root";
-$pasaword="";
+$username="Aprendiz";
+$password="Andres2004";
 
-    $con=  new mysqli("$servername", "$username", "$pasaword", "$database");
+    $con=  new mysqli("$servername", "$username", "$password", "$database");
 
     if($con -> connect_error ) {
         die("FALLA EN LA CONEXION " . $con -> connect_error) ; 
     }
-
 ?>
